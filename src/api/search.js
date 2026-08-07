f0f1bd265b7e3aae04931494939a7d589e65728c
Rayssa <rayssarrsilva1@gmail.com>
2026-08-07 15:37:56 -0300

    Function to reaturn the location requried

--- src/api/search.js
+++ src/api/search.js
@@ -1,5 +1,17 @@
-export async function searchLocation(query) {
+export async function searchLocation(query){
     if (!query.trim()) {
         return [];
     }
+
+    const url = `https://geocoding-api.open-meteo.com/v1/search?name= ${encodeURIComponent(query)}&count=5&language=en&format=json`;
+
+    const response = await fetch(url);
+
+    if (!response.ok){
+        throw Error("Failed to search locations");
+    }
+
+    const data = await response.json();
+
+    return data.results || [];
 }
