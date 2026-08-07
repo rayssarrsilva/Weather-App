2db25729b2da24aeeb78f893f2d5076584ca66f4
Rayssa <rayssarrsilva1@gmail.com>
2026-08-07 15:33:25 -0300

    Function to return the weather of a location

--- src/api/client.js
+++ src/api/client.js
@@ -1,18 +1,13 @@
 const KEY = process.env.KEY;
 
-export default function getWeather() {
-  return fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK/last30days?key=${KEY}`
-  )
-    .then((response) => {
-      console.log(response.status);
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-      return data;
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+export async function getWeather(location) {
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(location)}/?key=${KEY}`
+    
+    const response = await fetch(url);
+
+    if (!response.ok){
+        throw new Error("Failed to fetch weather data");
+    }
+
+    return response.json();
 }
---
+++ src/api/search.js
@@ -0,0 +1,5 @@
+export async function searchLocation(query) {
+    if (!query.trim()) {
+        return [];
+    }
+}
