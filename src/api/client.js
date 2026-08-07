1b2d247c20ced63818132f7dd4afcec0224812ac
Rayssa <rayssarrsilva1@gmail.com>
2026-08-07 19:05:06 -0300

    Correct the client fetch sintax

--- src/api/client.js
+++ src/api/client.js
@@ -2,9 +2,7 @@ const KEY = process.env.KEY;
 
 export async function getWeather(location) {
   const url =
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/` +
-    `${encodeURIComponent(location)}` +
-    `?unitGroup=us&key=${KEY}&contentType=json`;
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(location)}?unitGroup=us&key=${KEY}&contentType=json`;
 
   const response = await fetch(url);
 
