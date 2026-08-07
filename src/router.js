ceada56a0b2d6857cb63105023f0ab6797ad9d15
Rayssa <rayssarrsilva1@gmail.com>
2026-08-07 16:28:34 -0300

    Add the functions to the router
    decides wich part of the interface will be show

---
+++ src/router.js
@@ -0,0 +1,14 @@
+import Home from "./ui/Home.js";
+import Location from "./ui/Location.js";
+
+const app = document.querySelector("#content");
+
+export function showHome() {
+  app.innerHTML = "";
+  app.appendChild(Home());
+}
+
+export function showLocation(location) {
+  app.innerHTML = "";
+  app.appendChild(Location(location));
+}
